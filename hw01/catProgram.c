8da97c4eab5ab19b3fdd729a6654f80e880637f7
rlowman <robertlowman@kings.edu>
2016-09-20 01:14:03 -0400

    final commit for hw01

--- hw01/catProgram.c
+++ hw01/catProgram.c
@@ -8,16 +8,15 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
 
 int main(int argc, char * argv[]) { 
-  int outFile;
   char buffer[BUFFER_SIZE + 1];
   buffer[BUFFER_SIZE] = '\0';
   int numRead;
   int lineCount = 1;
-  int returnVal;
   int n = 0;
   int fileCount = argc - 1;
 
@@ -30,28 +29,30 @@ int main(int argc, char * argv[]) {
   int skip = 0;
   int i = 1;
   while(i <= fileCount && skip == 0) {
-    int temp;
-    if(strcmp(argv[i], "-") == 0) {
-      temp = open(STDIN_FILENO, O_RDONLY);
-    }
-    else {
+    int temp = STDIN_FILENO;
+    if(strcmp(argv[i], "-") != 0) { 
       temp = open(argv[i], O_RDONLY);
     }
     if(temp == -1) {         //If the given file was invalid, then give error
-      printf("Error opening input file");
-      skip = 1;
-      returnVal = 2;
+      perror("Error: ");
+      return 2;
     }
     else {
       numRead = read(temp, buffer, BUFFER_SIZE);
-      printf("%d", numRead);
       while(numRead > 0) {
 	if(n == 1) {
-	  char * p;
-	  p = strchr(buffer, '/n');
-	  while(p!=NULL) {
-	    char temp[
-	    p = strchr(p+1, 
+	  char * newline;
+	  char * start = buffer[0];
+	  newline = strchr(buffer, '\n');
+	  while(newline!=NULL) {
+	    int difference = newline - start;
+	    char tempBuf[difference + 1];
+	    tempBuf[difference] = '\0';
+	    strncpy(tempBuf, buffer, difference);
+	    printf("%d  %s", lineCount, tempBuf);
+	    lineCount = lineCount + 1;
+	    start = newline + 1;
+	    newline = strchr(buffer, '\n');
 	  }
         }
         else {
