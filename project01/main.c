2e0e0d810172d80c90ef0245f9dcb63f9ae06aa9
rlowman <robertlowman@kings.edu>
2016-10-05 11:52:51 -0400

    added dispatcher thread code

--- project01/main.c
+++ project01/main.c
@@ -5,10 +5,16 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <checksum-client.c>
-#include <proj01-networking.c>
 #include <proj01-networking.h>
 
+// The port number of the port we are requesting information from.
+int port;
+
+/**
+ * Sets the new worker thread count when requested by the user.
+ * 
+ * @return the new amount of worker threads to use.
+ **/
 int selectThreadCount() {
   int * returnValue;
   printf("Enter the amount of worker threads you wish to use: ");
@@ -16,6 +22,11 @@ int selectThreadCount() {
   return returnValue;
 }
 
+/**
+ * Sets the new port number when requested by the user.
+ *
+ * @return the new port number to use.
+ **/
 int selectPort() {
   int * returnValue;
   printf("Enter the port number you wish to use: ");
@@ -23,9 +34,19 @@ int selectPort() {
   return returnValue;
 }
 
+/**
+ * 
+ **/
+void * initialize_to_server(void * index) {
+  int listenSocket = server_initialize_networking(port);
+
+  while(true) {
+    
+  }
+}
+
 int main(int argc, char * argv[]) {
   int threadDefault = 4;
-  int port; // Set to default here
   if(argc == 2) {
     if(strcmp(argv[1], "-p") == 0) {
       port = selectPort();
@@ -45,8 +66,19 @@ int main(int argc, char * argv[]) {
     }
   }
 
-  pthread_t threads[threadDefault];
-  while(true) {
+  pthread_t dispatcherThread;
+  pthread_t workerThreads[threadDefault];
+  int count = 0;
+
+  int status = pthread_create(&dispatcherThread, NULL,
+			      initialize_to_server, (void *)i);
+
+  if(status != 0) {
+    printf("Error %d creating dispather thread. \n", status);
+    exit(1);
+  }
+  long i;
+  for(i = 0; i < threadDeafult; i++) {
     
   }
   return 0;
